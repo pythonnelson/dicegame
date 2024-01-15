78f35df95547a9d752a165ed3297f56ea2373bbc
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2024-01-14 18:25:30 -0800

    Display current score for player 1

--- script.js
+++ script.js
@@ -3,9 +3,33 @@
 // Select the element
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-const dice = document.querySelector('.dice');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Initial values
 score0El.textContent = 0;
 score1El.textContent = 0;
-dice.classList.add('hidden');
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  // Each time we roll the dice, generate a new number
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    // Add the current score to the player
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to the next player
+  }
+});
